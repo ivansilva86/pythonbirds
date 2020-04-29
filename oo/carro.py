1be0b087761aa994012f8e16682588245a907766
ivansilva86 <63621288+ivansilva86@users.noreply.github.com>
2020-04-29 16:56:37 -0300

    Implementada a direção

--- oo/carro.py
+++ oo/carro.py
@@ -91,6 +91,34 @@ Ela oferece os seguintes atributos:
     'Oeste'
 """
 
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_a_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_a_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        return self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        return self.direcao.girar_a_esquerda()
+
+    pass
+            
+
+
+
 NORTE = 'Norte'
 SUL = 'Sul'
 LESTE = 'Leste'
